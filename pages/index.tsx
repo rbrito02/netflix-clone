dea5368da9da9af9d3523a271205fff86fad9292
Raudy Brito <raudybrito@knights.ucf.edu>
2023-07-25 15:38:03 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import useCurrentUser from '@/hooks/userCurrentUser';
+import {NextPageContext} from 'next';
+import {getSession, signOut} from 'next-auth/react';
+
+import React from 'react';
+
+export async function getServerSideProps(context: NextPageContext) {
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/auth',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
+}
+
+export default function Home() {
+	const {data: user} = useCurrentUser();
+
+	return (
+		<>
+			<h1 className="text-2xl text-green-300">Netflix Clone</h1>
+			<p className="text-white">Logged in as : {user?.name}</p>
+			<button onClick={() => signOut()} className="h-10 w-full bg-white">
+				LogOut!
+			</button>
+		</>
+	);
+}
