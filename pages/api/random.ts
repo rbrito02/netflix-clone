4652da8a1e6668df41ba83d5c4991082996d0994
Raudy Brito <raudyb02@gmail.com>
2023-08-12 21:05:57 -0400

    created navbar for desktop and mobile

---
+++ pages/api/random.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const random = async (req: NextApiRequest, res: NextApiResponse) => {};
+
+export default random;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,39 @@
-import useCurrentUser from '@/hooks/userCurrentUser';
-import {NextPageContext} from 'next';
-import {getSession, signOut} from 'next-auth/react';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
 
 import React from 'react';
+import useCurrentUser from '@/hooks/userCurrentUser';
 
 export async function getServerSideProps(context: NextPageContext) {
-	const session = await getSession(context);
+  const session = await getSession(context);
 
-	if (!session) {
-		return {
-			redirect: {
-				destination: '/auth',
-				permanent: false,
-			},
-		};
-	}
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
 
-	return {
-		props: {},
-	};
+  return {
+    props: {},
+  };
 }
 
 export default function Home() {
-	const {data: user} = useCurrentUser();
+  const { data: user } = useCurrentUser();
 
-	return (
-		<>
-			<h1 className="text-2xl text-green-300">Netflix Clone</h1>
-			<p className="text-white">Logged in as : {user?.name}</p>
-			<button onClick={() => signOut()} className="h-10 w-full bg-white">
-				LogOut!
-			</button>
-		</>
-	);
+  return (
+    <>
+      <h1 className="text-2xl text-green-300">Netflix Clone</h1>
+      <p className="text-white">
+        Logged in as :
+        {user?.name}
+      </p>
+      <button onClick={() => signOut()} className="h-10 w-full bg-white">
+        LogOut!
+      </button>
+    </>
+  );
 }
